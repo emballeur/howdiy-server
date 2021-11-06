b4d09f773d4914f01360172d3dcaf9bfa45462a4
whatkimkong <whatkimkong@gmail.com>
2021-11-06 15:49:43 +0100

    worked on auth.routes and User model

--- models/User.model.js
+++ models/User.model.js
@@ -3,11 +3,18 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    firstName: String,
+    lastName: String,
+    email: {
+      type: String,
+      unique: true,
+    },
     username: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      unique: true,
     },
     password: String,
+    favourites: [String],
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
